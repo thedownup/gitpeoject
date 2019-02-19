e7fd3a8905424e16370a9e29525ed95d5572b236
zjt <632443784@qq.com>
2019-02-19 14:55:17 +0800

    git test

---
+++ src/main/java/com/git/Main.java
@@ -0,0 +1,12 @@
+package com.git;
+
+/**
+ * @description:
+ * @author: zjt
+ * @date: 2019-02-19 13:37
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("test git!");
+    }
+}
